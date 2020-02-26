3d0f1a2da868ffec384e9c713b15e43b7a9f9170
Brandon Falter <brandon@voie.com>
2020-02-26 18:21:23 +0800

    Creating 2 models and controllers that is still in progress

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,41 @@
+class ReviewsController < ApplicationController
+
+  def index
+    raise
+    @reviews = Review.all
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to review_path(restaurant_id)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating Restaurants...'
+restaurant_attributes = [
+  {
+    name: 'Bali Buda',
+    address: 'Kerokoban, Bali Balong Regency',
+    phone_number: '+62 881 331 889980',
+    category: 'Indonesian'
+  },
+
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '+62 831 441 93030',
+    category: 'Italian'
+  },
+
+  {
+    name: 'Avacado',
+    address: 'Denpassar',
+    phone_number: '+62 831343466030',
+    category: 'Asian'
+  },
+
+  {
+    name: 'Moana',
+    address: '789 Drive Maluu, Berlin',
+    phone_number: '+62 831 8768730',
+    category: 'Hawaiian'
+  },
+
+  {
+    name: 'Burger King',
+    address: 'Street , Balong',
+    phone_number: '+62 831 432330',
+    category: 'American'
+  }
+]
+
+Restaurant.create!(restaurant_attributes)
+
+puts 'Finished!'
